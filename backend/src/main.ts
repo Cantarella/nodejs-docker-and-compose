2d10c4ae58fe02197d03c0df58ed6c7b6602aa0b
Cantarella <zdanevich@progressivemedia.ru>
2024-07-03 09:07:43 +0300

    добавлено управление cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,12 @@ import { nestCsrf } from 'ncsrf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+      "origin": [/(cantarella)+.*\.ru$/],
+      "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+      "preflightContinue": false,
+      "optionsSuccessStatus": 204
+  });
   app.use(cookieParser());
   app.use(nestCsrf());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
